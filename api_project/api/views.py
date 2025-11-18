d525ced136774ac8fc8fb895a5942019a012cb48
Maruf <morufashiru@gmail.com>
2025-11-19 00:37:11 +0100

    Set up basic Book API with DRF

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()       # Get all Book objects
+    serializer_class = BookSerializer   # Use the serializer we created
+
